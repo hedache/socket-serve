12ac6c327dae7332ca62d85b7c16ae46ed9f062b
Diego Alejandro Moná Cataño <Diego.Mona@ibm.com>
2023-01-21 15:27:07 -0500

    :tada: First commit - rest serve

---
+++ README.md
@@ -0,0 +1,22 @@
+
+
+# Sockets-Server
+
+Reconstruir los módulo de Node, cualquier de estos 2 comandos
+```
+npm i
+npm install
+```
+
+
+Generar el DIST
+```
+tsc -w
+```
+
+
+Levantar servidor, cualquier de estos 2 comandos
+```
+nodemon dist/
+node dist/
+```
---
+++ index.ts
@@ -0,0 +1,26 @@
+import Server from "./class/server";
+import router from "./routes/router";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+
+
+const server = new Server();
+
+// BodyParser
+// Los middlewares son códigos que se ejecutan antes de que una petición HTTP llegue al manejador de rutas o antes de que un cliente reciba una respuesta,
+// basicamente es.. lo q envíen, tomalo y pasalo a json.
+server.app.use( bodyParser.urlencoded({ extended: true }) );
+server.app.use( bodyParser.json() );
+
+// CORS
+// permitir que cualquier persona pueda llamar mis servicios
+server.app.use( cors({ origin: true, credentials: true }) )
+
+// Rutas de servicio
+server.app.use('/', router)
+
+
+server.start(() => {
+    console.log(`El servidor corriendo en el puerto ${ server.port }`);
+})
